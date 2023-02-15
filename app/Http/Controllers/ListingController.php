645dcd16239e5fc0dbd2bf91ef03c90c5274ee73
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-15 22:15:22 +0100

    App commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    public function index()
+    {
+        $listings = Listing::latest()->filter(request(['tag', 'search']))->paginate(4);
+
+        return view('listings.index', [
+            'listings' => $listings
+        ]);
+    }
+
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    public function create()
+    {
+        return view('listings/create');
+    }
+
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Job was created successfully');
+    }
+}
---
+++ app/Models/Listing.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+
+    // protected $fillable = ['title', 'description', 'company', 'email', 'tags', 'location', 'website'];
+
+    public function scopeFilter($query, array $filters) {
+        if($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        if($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                  ->orWhere('company', 'like', '%' . request('search') . '%')
+                  ->orWhere('location', 'like', '%' . request('search') . '%');
+        }
+    }
+}
