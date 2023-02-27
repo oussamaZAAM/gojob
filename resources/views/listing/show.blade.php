94f2a19655744f60f2649999572946ca91e5d086
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-28 00:58:57 +0100

    Remove Edit and Delete buttons from show

--- resources/views/listing/show.blade.php
+++ resources/views/listing/show.blade.php
@@ -35,7 +35,7 @@
                 </div>
             </div>
         </x-card>
-        <x-card class="mt-4 p-2 flex space-x-6">
+        {{-- <x-card class="mt-4 p-2 flex space-x-6">
             <a href="/listing/{{$listing->id}}/edit">
                 <i class="fa-solid fa-pencil"></i> Edit
             </a>
@@ -46,6 +46,6 @@
                     <div class="text-red-500"><i class="fas fa-trash"></i> Delete</div>
                 </button>
             </form>
-        </x-card>
+        </x-card> --}}
     </div>
 </x-layout>
