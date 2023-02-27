7ecdd1de87fe86076e1d7331137cb69b3d0b4ef1
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-28 00:59:43 +0100

    Fix Syntax

--- resources/views/listing/create.blade.php
+++ resources/views/listing/create.blade.php
@@ -95,7 +95,7 @@
 
             <div class="mb-6">
                 <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-                    Create Gig
+                    Create a Job
                 </button>
 
                 <a href="/" class="text-black ml-4"> Back </a>
