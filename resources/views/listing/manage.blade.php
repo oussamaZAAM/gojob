91de899bd43742c76d9596a2052aa2e71be0ae5e
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-28 00:59:11 +0100

    Manage Page

---
+++ resources/views/listing/manage.blade.php
@@ -0,0 +1,58 @@
+<x-layout>
+    <x-card class="p-10">
+        <header>
+            <h1
+                class="text-3xl text-center font-bold my-6 uppercase"
+            >
+                Manage Gigs
+            </h1>
+        </header>
+
+        <table class="w-full table-auto rounded-sm">
+            <tbody>
+                @unless($listings->isEmpty())
+                @forEach($listings as $listing)
+                <tr class="border-gray-300">
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a href="/listing/{{$listing->id}}">
+                            {{$listing->title}}
+                        </a>
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <a
+                            href="/listing/{{$listing->id}}/edit"
+                            class="text-blue-400 px-6 py-2 rounded-xl"
+                            ><i
+                                class="fa-solid fa-pen-to-square"
+                            ></i>
+                            Edit</a
+                        >
+                    </td>
+                    <td
+                        class="px-4 py-8 border-t border-b border-gray-300 text-lg"
+                    >
+                        <form method="POST" action="/listing/{{$listing->id}}" >
+                            @csrf
+                            @method('DELETE')
+                            <button>
+                                <div class="text-red-500"><i class="fas fa-trash"></i> Delete</div>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforEach
+                @else
+                <tr class="vorder-gray-300">
+                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                        <p class="text-center">No Listing Found</p>
+                    </td>
+                </tr>
+                @endunless
+            </tbody>
+        </table>
+    </x-card>
+</x-layout>
