8e53567213f4ca2cae53a602492744c3cf10d691
oussamaZAAM <zaam.oussama@gmail.com>
2023-02-28 01:00:13 +0100

    Foreign key between User and Listing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // User::factory(5)->create();
 
-        Listing::factory(5)->create();
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@doe.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id,
+            
+        ]);
 
         // Listing::create([
         //     'title' => 'Laravel Senior Developer',
