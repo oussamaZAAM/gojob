738c2e7dfd918dd09caabaf3b3aec561ac70a4fe
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-11 17:00:54 +0000

    Restrict authenticated users from Register Hero

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -4,15 +4,23 @@
 
     <div class="z-10">
         <h1 class="text-6xl font-bold uppercase text-white">
-            Lara<span class="text-black">Jobs</span>
+            GO<span class="text-black">Job</span>
         </h1>
         <p class="text-2xl text-gray-200 font-bold my-4">
-            Find or post Laravel jobs & projects
+            Find or post Web Development jobs & projects
         </p>
-        <div>
-            <a href="register.html"
-                class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
-                Up to List a Job</a>
-        </div>
+        @auth
+            <div>
+                <a href="/listing/create"
+                    class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Click
+                    to add a Job</a>
+            </div>
+        @else
+            <div>
+                <a href="/register"
+                    class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
+                    Up to List a Job</a>
+            </div>
+        @endAuth
     </div>
 </section>
